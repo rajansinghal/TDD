49b1200241240c54c1c7e042cf972a090a50624a
Rajan Singhal <mr.rajansinghal@gmail.com>
2018-01-06 18:48:02 +0530

    initial commit

---
+++ src/main/java/com/coding/tdd/PrimeFactors.java
@@ -0,0 +1,7 @@
+package com.coding.tdd;
+
+/**
+ * Created by rsinghal on 1/6/2018.
+ */
+public class PrimeFactors {
+}
---
+++ src/test/java/com/coding/tdd/PrimeFactorsTest.java
@@ -0,0 +1,22 @@
+package com.coding.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rsinghal on 1/6/2018.
+ */
+public class PrimeFactorsTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+
+    @Test
+    public  void  shouldReturnPrimeFactors() {}
+}

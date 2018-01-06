24ad621002a5826c0595ecafe56943bc4cb7634f
Rajan Singhal <mr.rajansinghal@gmail.com>
2018-01-06 19:31:57 +0530

    adding failing test case

--- src/test/java/com/coding/tdd/PrimeFactorsTest.java
+++ src/test/java/com/coding/tdd/PrimeFactorsTest.java
@@ -28,6 +28,6 @@ public class PrimeFactorsTest {
         assertEquals(asList(3), of(3));
         assertEquals(asList(2,2), of(4));
         assertEquals(asList(5), of(5));
-        assertEquals(asList(3,2), of(5));
+        assertEquals(asList(2,3), of(6));
     }
 }
